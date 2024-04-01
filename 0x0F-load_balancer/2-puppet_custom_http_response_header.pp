de076a7519f61a012fa7d4c26d6976759052c6a7
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-04-02 00:39:56 +0200

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,54 +1,25 @@
 # Puppet to add special header to web-server
 
-exec { 'update':
-  command => '/usr/bin/apt-get update',
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
-# Install nginx
-package { 'nginx':
-  ensure => installed,
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
 }
 
-# Create directory and files
-file { '/etc/nginx/html':
-  ensure => directory,
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
 }
 
-file { '/etc/nginx/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-}
-
-file { '/etc/nginx/html/404.html':
-  ensure  => file,
-  content => "Ceci n'est pas une page",
-}
-
-# Configure nginx
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => '
-server {
-    listen 80;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /etc/nginx/html;
-    index  index.html index.htm;
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /etc/nginx/html;
-      internal;
-    }
-}',
-  notify  => Service['nginx'],
-}
-
-# Restart nginx service
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
