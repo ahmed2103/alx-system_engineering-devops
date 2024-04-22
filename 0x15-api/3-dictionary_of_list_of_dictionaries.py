30d87b4088cb8bc9e998af898259b55a7efe1578
Hasona <hasona@kali.kali>
2024-04-22 23:56:53 +0200

    test

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,11 +7,9 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = get(url + "users/").json()
     todos = get(url + "todos").json()
-    data = {}
-    for user in users:
-        data[user.get("id")] = [{"username": user.get("username"),
-                                 "task": task.get("title"),
-                                 "completed": task.get("completed")}
-                                for task in todos if task.get("userId") == user.get("id")]
     with open("todo_all_employees.json", 'w') as jsonfile:
-        dump(data, jsonfile)
+        dump({user.get("id"): [{"task": task.get("title"),
+                                "completed": task.get("completed"),
+                                "username": user.get("username")} for task in todos
+                               if user.get("id") == task.get("userId")]
+              for user in users}, jsonfile)
