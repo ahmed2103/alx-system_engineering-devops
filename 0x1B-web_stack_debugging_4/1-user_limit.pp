070a892d2e914a49aabb7aff7e99a5c311823fe5
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-05-16 11:24:10 +0300

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
 # Puppet manifesto to fix user maximum file limit
 
-exec {'replace1':
+exec {'replace-1':
   provider => shell,
   command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
   before   => Exec['replace-2'],
 }
 
-exec {'replace2':
+exec {'replace-2':
   provider => shell,
   command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
